265fb2541ae68a88674f41346b2827bf5b0c6870
raszidzie <charliescene512@gmail.com>
2019-09-21 15:37:04 -0400

    signup-with-confirmation-link

---
+++ accounts/forms.py
@@ -0,0 +1,13 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignUpForm(UserCreationForm):
+    first_name = forms.CharField(max_length=100, help_text='Last Name')
+    last_name = forms.CharField(max_length=100, help_text='Last Name')
+    email = forms.EmailField(max_length=150, help_text='Email')
+
+
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
